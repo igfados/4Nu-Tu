428c23bafd24498da62fff0beee0f0d945a338c3
igfados <152076807+igfados@users.noreply.github.com>
2026-02-04 00:40:25 +0700

    JAVA.C

--- JAVA.C
+++ JAVA.C
@@ -1,3 +1,3 @@
-mnfs:dkmnrsm1ygmmu4tdftrrnc1Mu4tnbrng,krg0,_/pnmpng_|ygd!4ngkt0lh4lttrnprts1cprt1kpl,pcwt/trk
+mnfs:dkmnrsm1ygmmu4tdftrrnc1Mu4tnbrng,krg0,_/pnmpng_|ygd!4ngkt0lh4lt trnprts1cprt1kpl,pcwt/trk
 
 Dalam perdagangan internasional, “manifest” adalah daftar semua kargo yang diangkut pada pelayaran atau penerbangan laut tertentu. Manifes juga dikenal sebagai “deklarasi kargo”. Manifes memberikan informasi tentang aspek fisik kargo, seperti deskripsi, berat dan/atau ukuran.
